9d4031af643866106342e3f5df52b6c327b2844f
Imperial-Slug <111785134+Imperial-Slug@users.noreply.github.com>
2023-11-02 22:28:11 -0400

    Initial commit

---
+++ android/src/com/game/vssg/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.game.vssg;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.game.vssg.VSSG;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new VSSG(), config);
+	}
+}
