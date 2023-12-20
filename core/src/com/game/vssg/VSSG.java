8d8c569c219a94966123fed35c796727a7005bb9
Imperial-Slug <111785134+Imperial-Slug@users.noreply.github.com>
2023-12-20 11:28:52 -0500

    Added new sprite for larger, new ship

--- core/src/com/game/vssg/VSSG.java
+++ core/src/com/game/vssg/VSSG.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
